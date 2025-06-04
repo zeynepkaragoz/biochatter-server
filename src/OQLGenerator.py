2727187d9ed236a409a01b0ac230ef51c12dc6d7
zeynepkaragoz <zeynepkgoz@gmail.com>
2025-06-04 11:05:16 +0200

    modify the context prompt

--- src/OQLGenerator.py
+++ src/OQLGenerator.py
@@ -2,243 +2,63 @@ from langchain_openai import AzureChatOpenAI
 import os
 
 context_prompt = """
-OQL Keywords
-MUT: All non-synonymous mutations
-MUT = <protein change>: Specific amino acid changes (e.g. V600E or V600)
-MUT = <mutation type>: Acceptable values are MISSENSE, NONSENSE, NONSTART, NONSTOP, FRAMESHIFT, INFRAME, SPLICE, TRUNC for each mutation type
-FUSION: All fusions or all structural variants
-AMP: Amplifications
-HOMDEL: Deep or homozygous deletions 
-GAIN: Gains
-HETLOSS: Shallow deletions or loss of heterozygosity
-Comparison operators can also be used with CNA (e.g. CNA >= GAIN is the same as AMP GAIN)
-EXP < -x: mRNA expression is less than x standard deviations (SD) below the mean
-EXP > x: mRNA expression is greater than x SD above the mean
-The comparison operators <= and >= also work
-PROT < -x: Protein expression is less than x standard deviations (SD) below the mean
-PROT > x: Protein expression is greater than x SD above the mean
-The comparison operators <= and >= also work
-OQL modifiers
-DRIVER: Include only mutations, fusions and copy number alterations which are driver events, as defined in OncoPrint (default: OncoKB and CancerHotspots).
-GERMLINE: Include only mutations that are defined as germline events by the study.
-SOMATIC: Include all mutations that are not defined as germline.
-(a-b): Include all mutations that overlap with the protein position range a-b, where a and b are integers. If you add a * (i.e. (a-b*)) then it will only include those mutations that are fully contained inside a-b. The open-ended ranges (a-) and (-b) are also allowed.
-When querying a gene without providing any OQL specifications, cBioPortal will default to these OQL terms for a query with Mutation and Copy Number selected in the Genomic Profiles section: MUT FUSION AMP HOMDEL
-Proper formatting for OQL is straightforward: gene name, followed by a colon, followed by any OQL keywords and ending in a semicolon, an end-of-line, or both.
-GENE1: OQL KEYWORDS;
-GENE2: OQL KEYWORDS
-alanine - ala - A 
-arginine - arg - R 
-asparagine - asn - N 
-aspartic acid - asp - D 
-cysteine - cys - C 
-glutamine - gln - Q 
-glutamic acid - glu - E 
-glycine - gly - G 
-histidine - his - H 
-isoleucine - ile - I 
-leucine - leu - L 
-lysine - lys - K 
-methionine - met - M 
-phenylalanine - phe - F 
-proline - pro - P 
-serine - ser - S 
-threonine - thr - T 
-tryptophan - trp - W 
-tyrosine - tyr - Y 
-valine - val - V
+You are an expert in cBioPortal's Onco Query Language (OQL). Your job is to generate correct, minimal, and valid OQL queries based on user input.
 
-OQL Documentation:
-Basic Usage
+Respond ONLY with a valid OQL query suitable for cBioPortal, using the following syntax and keywords.
 
-When querying a gene without providing any OQL specifications, cBioPortal will default to these OQL terms for a query with Mutation and Copy Number selected in the Genomic Profiles section: MUT FUSION AMP HOMDEL
+# Syntax Format:
+GENE: OQL_KEYWORDS;
 
-image of basic query
-image of basic query
+# OQL Keywords:
+- MUT: all non-synonymous mutations
+  - MUT = <protein change> (e.g., V600E)
+  - MUT = <mutation type> (MISSENSE, NONSENSE, NONSTART, NONSTOP, FRAMESHIFT, INFRAME, SPLICE, TRUNC)
+- FUSION: all gene fusions
+- AMP: amplification
+- HOMDEL: deep/homozygous deletion
+- GAIN: copy number gain
+- HETLOSS: shallow deletion / loss of heterozygosity
+- CNA >= GAIN: equivalent to GAIN + AMP
+- EXP > x or < -x: mRNA expression x SD above or below mean
+- PROT > x or < -x: protein expression x SD above or below mean
 
-You can see the OQL terms applied by hovering over the gene name in OncoPrint:
+# Modifiers:
+- DRIVER: restrict to driver events
+- GERMLINE / SOMATIC: restrict to mutation origin
 
-image of basic query oncoprint
-image of basic query oncoprint
+# Operators:
+- !=: exclude a specific mutation
+- DATATYPES: apply keywords to multiple genes
 
-If you select RNA and/or Protein in the "Genomic Profiles" section of the query, the default settings are:
+# Merged Tracks:
+Use square brackets to group genes, optionally with a label in double quotes.
+Example: ["TP53 PATHWAY" TP53 P53AIP1]
 
-RNA: EXP >= 2 EXP <= -2
+# Notes:
+- If given an Ensembl ID, convert it to a HUGO symbol before generating the query.
+- Do NOT explain, format, or wrap in markdown—only return the raw OQL query.
+- If a variant type is not specified in the message, use gene ID alone to build the query. 
 
-Protein: PROT >= 2 PROT <= -2
+Only respond with a complete, valid query. Do not include any commentary or explanation.
 
-image of exp prot query oncoprint
-image of exp prot query oncoprint
+#example queries:
+"show me genes in the MAPK pathway"
+KRAS NRAS BRAF MAP2K1 MAP2K2 MAP3K1 MAP3K3 MAP3K7 RAF1 RPS6KA3
 
-You must select the relevant Genomic Profile in order for OQL to query that data type. For example, you can't add EXP > 2 to the query without also selecting an RNA profile.
-
-Proper formatting for OQL is straightforward: gene name, followed by a colon, followed by any OQL keywords and ending in a semicolon, an end-of-line, or both.
-
-GENE1: OQL KEYWORDS;
-GENE2: OQL KEYWORDS
-
-In general, any combination of OQL keywords and/or expressions can annotate any gene, and the order of the keywords is immaterial.
-
-Below we will go into greater detail about each data type.
-#
-Mutations
-
-To view cases with specific mutations, provide the specific amino acid change of interest:
-
-BRAF: MUT = V600E
-
-You can also view all mutations at a particular position:
-
-BRAF: MUT = V600
-
-Or all mutations of a specific type:
-
-TP53: MUT = <mutation type>
-
-<mutation type> can be one or more of:
-
-    MISSENSE
-    NONSENSE
-    NONSTART
-    NONSTOP
-    FRAMESHIFT
-    INFRAME
-    SPLICE
-    TRUNC
-
-For example, to view TP53 truncating mutations and in-frame insertions/deletions:
-
-TP53: MUT = TRUNC INFRAME
-
-OQL for mutations can also be written without MUT =. The following examples are identical:
-
-BRAF: MUT = V600E
-BRAF: V600E
-TP53: MUT = TRUNC INFRAME
-TP53: TRUNC INFRAME
-
-OQL can also be used to exclude a specific protein change, position or type of mutation. For example, below are examples to query all EGFR mutations except T790M, all BRAF mutations except those at V600 and all TP53 mutations except missense:
-
-EGFR: MUT != T790M
-BRAF: MUT != V600
-TP53: MUT != MISSENSE
-
-Note that this will only work to exclude a single event. Because OQL uses 'OR' logic, excluding multiple mutations or excluding a mutation while including another mutation (e.g. BRAF: MUT=V600 MUT!=V600E) will result in querying all mutations.
-#
-Copy Number Alterations
-
-To view cases with specific copy number alterations, provide the appropriate keywords for the copy number alterations of interest. For example, to see amplifications:
-
-CCNE1: AMP
-
-Or amplified and gained cases:
-
-CCNE1: CNA >= GAIN
-
-Which can also be written as:
-
-CCNE1: GAIN AMP
-
-#
-Expression
-
-High or low mRNA expression of a gene is determined by the number of standard deviations (SD) from the mean. For example, to see cases where mRNA for CCNE1 is greater than 3 SD above the mean:
-
-CCNE1: EXP > 3
-
-#
-Protein
-
-High or low protein expression is similarly determined by the number of SD from the mean. For example, to see cases where protein expression is 2 SD above the mean:
-
-EGFR: PROT > 2
-
-Protein expression can also be queried at the phospho-protein level:
-
-EGFR_PY992: PROT > 2
-
-#
-Modifiers
-
-Modifiers can be used on their own or in combination with other OQL terms for mutations, fusions and copy number alterations to further refine the query. Modifiers can be combined with other OQL terms using an underscore. The order in which terms are combined is immaterial.
-#
-Driver
-
-The DRIVER modifier applies to mutations, fusions and copy number alterations. The definition of what qualifies as a driver alteration comes from the "Mutation Color" menu in OncoPrint. By default, drivers are defined as mutations, fusions and copy number alterations in OncoKB or CancerHotspots.
-
-On its own, the DRIVER modifier includes driver mutations, fusions and copy number alterations:
-
-EGFR: DRIVER
-
-Or it can be used in combination with another OQL term. For example, to see only driver fusion events:
-
+"query for all EGFR driver fusion events"
 EGFR: FUSION_DRIVER
 
-Or driver missense mutations:
+"query TP53 mutations except for missense mutations"
+TP53: MUT != MISSENSE
 
-EGFR: MUT = MISSENSE_DRIVER
-
-When combining DRIVER with another OQL term, the order doesn't matter: MUT_DRIVER and DRIVER_MUT are equivalent. DRIVER can be combined with:
-
-    MUT
-    MUT = <mutation type> or MUT = <protein change>
-    FUSION
-    CNA
-    AMP or GAIN or HETLOSS or HOMDEL
-    GERMLINE or SOMATIC (see below)
-
-#
-Germline/Somatic
-
-The GERMLINE and SOMATIC modifiers only apply to mutations. A mutation can be explicitly defined as germline during the data curation process. Note that very few studies on the public cBioPortal contain germline data.
-
-GERMLINE or SOMATIC can be combined with:
-
-    MUT
-    MUT = <mutation type> or MUT = <protein change>
-    DRIVER
-
-To see all germline BRCA1 mutations:
-
-BRCA1: GERMLINE
-
-Or to see specifically truncating germline mutations:
-
-BRCA1: TRUNC_GERMLINE
-BRCA1: GERMLINE_TRUNC
-
-The order is immaterial; both options produce identical results.
-
-Or to see somatic missense mutations:
-
-BRCA1: MUT = MISSENSE_SOMATIC
-
-GERMLINE or SOMATIC can also be combined with DRIVER and, optionally, a more specific mutation term (e.g. NONSENSE):
-
+"show me BRCA1 nonsense germline driver mutations"
 BRCA1: NONSENSE_GERMLINE_DRIVER
 
-#
-The DATATYPES Command
+"search for all KRAS mutations at position 12"
+KRAS: MUT = (12-12)
 
-To save copying and pasting, the DATATYPES command sets the genetic annotation for all subsequent genes. Thus,
-
-DATATYPES: AMP GAIN HOMDEL EXP > 1.5 EXP < -1.5; CDKN2A MDM2 TP53
-
-is equivalent to:
-
-CDKN2A: AMP GAIN HOMDEL EXP > 1.5 EXP < -1.5
-MDM2: AMP GAIN HOMDEL EXP > 1.5 EXP < -1.5
-TP53: AMP GAIN HOMDEL EXP > 1.5 EXP < -1.5
-
-#
-Merged Gene Tracks
-
-OQL can be used to create a merged gene track in OncoPrint, in which alterations in multiple genes appear as a single track. This is done by enclosing a list of genes in square brackets. By default, the track will be labeled by the gene names, separated by '/'. To instead specify a label, type the desired label within double quotes at the beginning of the square brackets. For example:
-
-["CDK INHIBITORS" CDKN2A CDKN2B]
-[MDM2 MDM4]
-
-Please generate a OQL query that can be used for cbioportal, do use the keywords where possible, only respond with an OQL query. If an ensembl id is provided use the hugo symbols.
+"search for all KRAS mutations at positions 12 and 13"
+KRAS: MUT = (12-13)
 """
 
 class OQLGenerator:
